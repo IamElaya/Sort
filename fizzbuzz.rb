aca0ae701a0e0138fbcfc78deb266cd3d9ddaef8
IamElaya <asison13@gmail.com>
2016-01-06 01:29:52 +0000

    first commit

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,24 @@
-1.upto(100) do |i|
-  if i % 5 == 0 && i % 3 == 0
-    puts "FizzBuzz"
-  elsif i % 5 == 0
-    puts "Buzz"
-  elsif i % 3 == 0
-    puts "Fizz"
-  else
-    puts i
+#1.upto(100) do |i|
+def fizzbuzz(range)
+  range.each do |i|
+  #if i % 5 == 0 && i % 3 == 0
+   # puts "FizzBuzz"
+  #elsif i % 5 == 0
+   # puts "Buzz"
+  #elsif i % 3 == 0
+   # puts "Fizz"
+  #else
+   # puts i
+  #end
+  fizz = (i % 3 == 0)
+  buzz = (i % 5 ==0)
+  puts case 
+       when fizz && buzz then "FizzBuzz"
+       when fizz then "Fizz"
+       when buzz then "Buzz"
+       else i   
   end
 end
+end
+
+puts fizzbuzz(77..619)
